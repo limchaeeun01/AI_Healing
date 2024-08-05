aa5e2a38854db5d590f3083dba692e3d3aaee01f
chaeni_beam <twokids1005@naver.com>
2024-08-05 17:37:07 +0900

    커밋테스트

--- app/src/main/java/com/chaeni__beam/ai_healing/MainActivity.kt
+++ app/src/main/java/com/chaeni__beam/ai_healing/MainActivity.kt
@@ -41,7 +41,7 @@ class MainActivity : AppCompatActivity() {
         )
         navView.setupWithNavController(navController)
 
-        currentEmotion = intent?.getStringExtra("emotion") ?: "sadness" //테스트
+        currentEmotion = intent?.getStringExtra("emotion") ?: "sadness" //테스트 테스트용
 
         if (savedInstanceState == null) {
             val bundle = Bundle().apply {
